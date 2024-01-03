3a93197627d97ff8f595e389f1f5fa8297f9399c
Joel M. Gonzalez <joelm12pr@gmail.com>
2024-01-03 18:07:03 -0500

    created a dropup for language selection

--- client/src/components/Editor.vue
+++ client/src/components/Editor.vue
@@ -2,6 +2,17 @@
     <div>
         <pre id="editor"></pre>
         <button type="button" @click="submitCode" id="submitButton" class="btn btn-submit btn-lg">Submit</button>
+        <div id="dropdown-container" class="dropup">
+            <button id="language-dropdown" class="btn btn-custom dropdown-toggle btn-lg" type="button"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Language
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">C++</a></li>
+                <li><a class="dropdown-item" href="#">Python</a></li>
+                <li><a class="dropdown-item" href="#">JavaScript</a></li>
+            </ul>
+        </div>
     </div>
 </template>
   
@@ -53,7 +64,7 @@ export default {
                 language: mode,
                 code: this.editor.session.getValue(),
             };
-
+            dropdown - container
             axios.post(path, requestData)
                 .then((res) => {
                     this.submissionResult = res;
@@ -82,6 +93,7 @@ export default {
     bottom: 0;
     left: 0;
     right: 0;
+    padding: 1% 3%;
     border-radius: 1.5%;
 }
 
@@ -95,5 +107,27 @@ export default {
     border-color: black;
     background-color: #F1FA8C;
     padding: 1% 3%;
+}
+
+#dropdown-container {
+    position: absolute;
+    top: 91.5%;
+    margin: 0%;
+    left: 78%;
+    z-index: 1%;
+}
+
+.btn-custom {
+    color: #282A36;
+    border-color: black;
+    background-color: #F1FA8C;
+    width: 20.1em;
+    height: 3.6em;
+}
+
+.dropdown-item {
+    color: #282A36;
+    background-color: #e4e6eb;
+    width: 25em;
 }
 </style>
