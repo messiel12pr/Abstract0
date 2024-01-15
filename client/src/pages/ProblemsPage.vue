7b6fdd7622faaa95678cb87ba817acbb33c46edf
Joel M. Gonzalez <joelm12pr@gmail.com>
2024-01-14 19:13:36 -0500

    Created ProblemsPage and ProblemSetComponent

---
+++ client/src/pages/ProblemsPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <NavBarComponent />
+    <div>
+
+    </div>
+</template>
+
+<script>
+import NavBarComponent from '../components/NavBarComponent.vue';
+import ProblemSet from '../components/ProblemSet.vue';
+
+export default {
+    components: {
+        NavBarComponent,
+        ProblemSet,
+    },
+};
+</script>
+
+<style></style>
