4b6f10e260c79a7cd004a1d283dd357297bc3ad4
Joel M. Gonzalez <joelm12pr@gmail.com>
2024-01-23 17:41:23 -0500

    Redirected login callback to home page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Editor from '../pages/EditorPage.vue';
 import EditorComponent from '../components/Editor.vue';
-import CallbackComponent from '../components/CallbackComponent.vue';
 import Home from '../pages/HomePage.vue';
 import Problems from '../pages/ProblemsPage.vue';
 
@@ -17,11 +16,6 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/editor',
-      name: 'editor',
-      component: Editor    
     },
     {
       path: '/problems',
@@ -42,7 +36,7 @@ const router = createRouter({
     {
       path: '/callback',
       name: 'callback',
-      component: CallbackComponent
+      component: Home
     },
   ]
 })
