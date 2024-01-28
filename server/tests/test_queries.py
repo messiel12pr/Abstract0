3ceb39a2d14dd4905798828881615655525ccb72
Joel M. Gonzalez <joelm12pr@gmail.com>
2024-01-27 21:32:48 -0500

    modified test queries

--- server/tests/test_queries.py
+++ server/tests/test_queries.py
@@ -14,9 +14,9 @@ def test_problem_creation():
     assert db.get_problem(1) == [(1, 1, "Append node to list", "/problems/linked_list", "Easy", "Use the tail of the linked list")]
 
 def test_user_creation():
-    db.create_user(12345, 'test_user')
-    assert db.get_user(12345) == [(12345, 'test_user')]
+    db.create_user(95717805, 'messiel12pr')
+    assert db.get_user(95717805) == [(95717805, 'messiel12pr')]
 
 def test_submission_details_creation():
-    db.create_submission_details(1, "Accepted", "2024-01-12", "0", 12345)
-    assert db.get_submission_details(12345) == [(1, 1, 'Accepted', datetime.date(2024, 1, 12), 0, 12345)]
+    db.create_submission_details(1, "Accepted", "2024-01-12", "0", 95717805)
+    assert db.get_submission_details(95717805) == [(1, 1, 'Accepted', datetime.date(2024, 1, 12), 0, 95717805)]
